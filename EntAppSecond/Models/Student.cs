1408c7f1c9fc16e3b44befdea0a6b8cc0ffc708b
Adrian O Donnell <S00190500@mail.itsligo.ie>
2019-02-27 22:13:48 +0000

    Update 2702

---
+++ EntAppSecond/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntAppSecond.Models
+{
+    public class Student
+    {
+        [Required]
+        [Display (Name = "Student I.D.")]
+        public string StudentID { get; set; } = "";
+
+        [Required]
+        [Display (Name = "First Name")]
+        public string FirstName { get; set; } = "";
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = "";
+    }
+}
